bf9335661ae3f075edec5cf97ef8f25f8cee94af
Vi Ta <vta666.vt@gmail.com>
2019-06-28 12:45:41 -0500

    Added sample database with autoincrement primary key.

--- app/src/main/java/com/example/halaljava/MainActivity.java
+++ app/src/main/java/com/example/halaljava/MainActivity.java
@@ -3,25 +3,29 @@ package com.example.halaljava;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import com.example.halaljava.database.Expense;
 import com.example.halaljava.database.FinanceBaseHelper;
 import com.example.halaljava.database.Finance;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     private int counter = 0;
+    private EditText itemName;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button addItemButton = findViewById(R.id.startButtonHome);
-        Button removeItemButton = findViewById(R.id.resetHomeButton);
-        Button createDBButton = findViewById(R.id.createDB);
+        Button addItemButton = findViewById(R.id.start_button);
+        Button removeItemButton = findViewById(R.id.reset_button);
+        Button createDBButton = findViewById(R.id.database_button);
         final TextView centerText = findViewById(R.id.centerText);
         final Finance finance = new Finance(getApplicationContext());
         createDBButton.setOnClickListener(new View.OnClickListener()
@@ -88,5 +92,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public EditText getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(EditText itemName) {
+        this.itemName = itemName;
     }
 }
--- app/src/main/java/com/example/halaljava/database/Expense.java
+++ app/src/main/java/com/example/halaljava/database/Expense.java
@@ -1,6 +1,14 @@
 package com.example.halaljava.database;
 
 public class Expense {
+    String itemName;
+    double amount;
+
     public Expense() {
+    }
+
+    public Expense(String s, double a) {
+        itemName = s;
+        amount = a;
     }
 }
